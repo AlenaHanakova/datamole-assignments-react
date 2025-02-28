10b53b52eb8d3f51618b90ad9d3f60b6e9e9dbc6
Alena <al.hanakova@gmail.com>
2025-02-28 17:16:32 +0100

    F1: set footer default values

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -3,4 +3,7 @@ import styled from "styled-components";
 export const List = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: flex-start; /* or center if you prefer */
+    padding: 10px;
+    margin: 10px;
 `;
